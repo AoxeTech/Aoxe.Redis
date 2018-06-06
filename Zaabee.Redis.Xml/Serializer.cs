eddecb99c544e31b2194c0ac7068fcda81f1b4aa
Mutuduxf <aeondxf@live.com>
2018-06-06 16:23:48 +0800

    Improve README

---
+++ Zaabee.Redis.Xml/Serializer.cs
@@ -0,0 +1,19 @@
+﻿using Zaabee.Json;
+using Zaabee.Redis.ISerialize;
+
+namespace Zaabee.Redis.Json
+{
+    public class Serializer : ISerializer
+    {
+        public byte[] Serialize<T>(T o)
+        {
+            return o == null ? new byte[0] : o.ToJson().SerializeUtf8();
+        }
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length == 0) return default(T);
+            return bytes.DeserializeUtf8().FromJson<T>();
+        }
+    }
+}
