2924287516713c8a692dea03d0d5e21446fb8f59
Mutuduxf <aeondxf@live.com>
2018-09-21 17:23:58 +0800

    Complete list sync methods

--- Zaabee.Redis.Abstractions/IZaabeeRedisClient.cs
+++ Zaabee.Redis.Abstractions/IZaabeeRedisClient.cs
@@ -54,7 +54,35 @@ namespace Zaabee.Redis.Abstractions
 
         #region list
 
-
+        T ListGetByIndex<T>(string key, long index);
+        
+        long ListInsertAfter<T>(string key, T pivot, T value);
+        
+        long ListInsertBefore<T>(string key, T pivot, T value);
+        
+        T ListLeftPop<T>(string key);
+        
+        long ListLeftPush<T>(string key, T value);
+        
+        long ListLeftPush<T>(string key, List<T> values);
+        
+        long ListLength(string key);
+        
+        List<T> ListRange<T>(string key, long start = 0, long stop = -1);
+        
+        long ListRemove<T>(string key, T value, long count = 0);
+        
+        T ListRightPop<T>(string key);
+        
+        T ListRightPopLeftPush<T>(string source, string destination);
+        
+        long ListRightPush<T>(string key, T value);
+        
+        long ListRightPush<T>(string key, List<T> values);
+        
+        void ListSetByIndex<T>(string key, long index, T value);
+        
+        void ListTrim(string key, long start, long stop);
 
         #endregion
 
--- Zaabee.Redis/ZaabeeRedisClient.cs
+++ Zaabee.Redis/ZaabeeRedisClient.cs
@@ -131,6 +131,91 @@ namespace Zaabee.Redis
 
         #endregion
 
+        #region set
+
+        
+
+        #endregion
+
+        #region list
+
+        public T ListGetByIndex<T>(string key, long index)
+        {
+            return _serializer.Deserialize<T>(_db.ListGetByIndex(key, index));
+        }
+
+        public long ListInsertAfter<T>(string key, T pivot, T value)
+        {
+            return _db.ListInsertAfter(key, _serializer.Serialize(pivot), _serializer.Serialize(value));
+        }
+
+        public long ListInsertBefore<T>(string key, T pivot, T value)
+        {
+            return _db.ListInsertBefore(key, _serializer.Serialize(pivot), _serializer.Serialize(value));
+        }
+
+        public T ListLeftPop<T>(string key)
+        {
+            return _serializer.Deserialize<T>(_db.ListLeftPop(key));
+        }
+
+        public long ListLeftPush<T>(string key, T value)
+        {
+            return _db.ListLeftPush(key, _serializer.Serialize(value));
+        }
+
+        public long ListLeftPush<T>(string key, List<T> values)
+        {
+            return _db.ListLeftPush(key, values.Select(value => (RedisValue) _serializer.Serialize(value)).ToArray());
+        }
+
+        public long ListLength(string key)
+        {
+            return _db.ListLength(key);
+        }
+
+        public List<T> ListRange<T>(string key, long start = 0, long stop = -1)
+        {
+            return _db.ListRange(key, start, stop).Select(value => _serializer.Deserialize<T>(value)).ToList();
+        }
+
+        public long ListRemove<T>(string key, T value, long count = 0)
+        {
+            return _db.ListRemove(key, _serializer.Serialize(value), count);
+        }
+
+        public T ListRightPop<T>(string key)
+        {
+            return _serializer.Deserialize<T>(_db.ListRightPop(key));
+        }
+
+        public T ListRightPopLeftPush<T>(string source, string destination)
+        {
+            return _serializer.Deserialize<T>(_db.ListRightPopLeftPush(source, destination));
+        }
+
+        public long ListRightPush<T>(string key, T value)
+        {
+            return _db.ListRightPush(key, _serializer.Serialize(value));
+        }
+
+        public long ListRightPush<T>(string key, List<T> values)
+        {
+            return _db.ListRightPush(key, values.Select(value => (RedisValue) _serializer.Serialize(value)).ToArray());
+        }
+
+        public void ListSetByIndex<T>(string key, long index, T value)
+        {
+            _db.ListSetByIndex(key, index, _serializer.Serialize(value));
+        }
+
+        public void ListTrim(string key, long start, long stop)
+        {
+            _db.ListTrim(key, start, stop);
+        }
+
+        #endregion
+
         #region hash
 
         public bool HashAdd<T>(string key, string entityKey, T entity)
