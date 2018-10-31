deaf1b03c907d4990ba841b02db454831114626c
Mutuduxf <aeondxf@live.com>
2018-10-31 10:37:36 +0800

    Recode

--- Zaabee.Redis.Json/Serializer.cs
+++ Zaabee.Redis.Json/Serializer.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Zaabee.Json;
+﻿using Zaabee.Json;
 using Zaabee.Redis.ISerialize;
 
 namespace Zaabee.Redis.Json
@@ -11,21 +10,10 @@ namespace Zaabee.Redis.Json
             return o == null ? new byte[0] : o.ToJson().SerializeUtf8();
         }
 
-        public Task<byte[]> SerializeAsync<T>(T o)
-        {
-            return o == null ? Task.FromResult(new byte[0]) : Task.Factory.StartNew(() => o.ToJson().SerializeUtf8());
-        }
-
         public T Deserialize<T>(byte[] bytes)
         {
             if (bytes == null || bytes.Length == 0) return default(T);
             return bytes.DeserializeUtf8().FromJson<T>();
-        }
-
-        public Task<T> DeserializeAsync<T>(byte[] bytes)
-        {
-            if (bytes == null || bytes.Length == 0) return Task.FromResult(default(T));
-            return Task.Factory.StartNew(() => bytes.DeserializeUtf8().FromJson<T>());
         }
     }
 }
