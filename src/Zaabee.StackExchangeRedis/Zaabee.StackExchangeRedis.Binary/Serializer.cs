b9a82912d5110e2764d42748c8098d0951f7b04c
Mutuduxf <aeondxf@live.com>
2020-02-07 17:04:08 +0800

    Recode

--- src/Zaabee.StackExchangeRedis/Zaabee.StackExchangeRedis.Binary/Serializer.cs
+++ src/Zaabee.StackExchangeRedis/Zaabee.StackExchangeRedis.Binary/Serializer.cs
@@ -1,32 +1,11 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+﻿using Zaabee.Binary;
 using Zaabee.StackExchangeRedis.ISerialize;
 
 namespace Zaabee.StackExchangeRedis.Binary
 {
     public class Serializer : ISerializer
     {
-        [ThreadStatic] private static BinaryFormatter _binaryFormatter;
-
-        public byte[] Serialize<T>(T o)
-        {
-            if (o is null) return new byte[0];
-            _binaryFormatter ??= new BinaryFormatter();
-            using var stream = new MemoryStream();
-            _binaryFormatter.Serialize(stream, o);
-            var bytes = new byte[stream.Length];
-            stream.Seek(0, SeekOrigin.Begin);
-            stream.Read(bytes, 0, bytes.Length);
-            return bytes;
-        }
-
-        public T Deserialize<T>(byte[] bytes)
-        {
-            if (bytes is null || bytes.Length is 0) return default;
-            using var stream = new MemoryStream(bytes);
-            _binaryFormatter ??= new BinaryFormatter();
-            return (T) _binaryFormatter.Deserialize(stream);
-        }
+        public byte[] Serialize<T>(T o) => o.ToBytes();
+        public T Deserialize<T>(byte[] bytes) => bytes.FromBytes<T>();
     }
 }
