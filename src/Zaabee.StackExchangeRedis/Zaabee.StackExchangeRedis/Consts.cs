4eb728bca48a0130ec1b3c27bd177274999526f0
XiaoFeiDu <romanchaos@163.com>
2022-06-15 12:20:39 +0800

    Add Consts

---
+++ src/Zaabee.StackExchangeRedis/Zaabee.StackExchangeRedis/Consts.cs
@@ -0,0 +1,12 @@
+namespace Zaabee.StackExchangeRedis;
+
+public static class Consts
+{
+    internal static List<Type> RedisValueTypes = new()
+    {
+        typeof(int),
+        typeof(long),
+        typeof(double),
+        typeof(bool)
+    };
+}
