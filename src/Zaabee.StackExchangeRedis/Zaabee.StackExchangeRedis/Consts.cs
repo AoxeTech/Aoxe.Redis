5ba02df1e8fed554a4b9f000bfb970d9ce818caa
Mutudu <aeondxf@live.com>
2024-02-14 22:25:38 +0800

    Remove unused consts

--- src/Zaabee.StackExchangeRedis/Zaabee.StackExchangeRedis/Consts.cs
+++
@@ -1,7 +0,0 @@
-namespace Zaabee.StackExchangeRedis;
-
-public static class Consts
-{
-    internal static List<Type> RedisValueTypes =
-        new() { typeof(int), typeof(long), typeof(double), typeof(bool) };
-}
