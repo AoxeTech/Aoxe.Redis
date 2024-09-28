f61be39a64b9b38bdd0eb363b43c4c3b306bc5bd
Mutudu <aeondxf@live.com>
2024-09-28 21:59:51 +0800

    Fix

--- src/Aoxe.StackExchangeRedis/AoxeRedisServiceProviderExtensions.cs
+++ src/Aoxe.StackExchangeRedis/AoxeRedisServiceProviderExtensions.cs
@@ -2,12 +2,12 @@
 
 public static class AoxeRedisServiceProviderExtensions
 {
-    public static IServiceCollection AddAoxeMongo(
+    public static IServiceCollection AddAoxeRedis(
         this IServiceCollection services,
         Func<AoxeStackExchangeRedisOptions> optionsFactory
     ) => services.AddSingleton<IAoxeRedisClient>(new AoxeRedisClient(optionsFactory));
 
-    public static IServiceCollection AddAoxeMongo(
+    public static IServiceCollection AddAoxeRedis(
         this IServiceCollection services,
         AoxeStackExchangeRedisOptions options
     ) => services.AddSingleton<IAoxeRedisClient>(new AoxeRedisClient(options));
